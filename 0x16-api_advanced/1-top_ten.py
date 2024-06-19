9a3b22f97272a786ee5f48b83c7d0ac50712516c
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-19 05:45:21 +0300

    Script that queries Reddit API to return top 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module that queries Reddit API to print first 10 hot posts for a given sub
+"""
+
+import requests
+
+headers = {'User-Agent': 'My User Agent 1.0'}
+
+
+def top_ten(subreddit):
+    """
+    Returns top 10 posts for {subreddit}
+    """
+    resp = requests.get(
+        f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10',
+        allow_redirects=False,
+        headers=headers,
+        timeout=120
+    )
+    if resp.status_code == 200:
+        for post in resp.json()['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
